93e3146f64bac3d10a57303062e7fc23530dc7ad
Ralph Sliger <ralphsliger1@gmail.com>
2021-12-16 23:46:04 -0500

    Refactoring Game 1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 public class TennisGame1 implements TennisGame {
     
@@ -7,70 +8,72 @@ public class TennisGame1 implements TennisGame {
     private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1Name = "player1";
+        this.player2Name = "player2";
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals(player1Name))
             m_score1 += 1;
-        else
+        else if (playerName.equals(player2Name))
             m_score2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (sameScore())
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+           score = areEquals(m_score1);
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (isOver4())
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = over4Score(getMinusResult(m_score1,m_score2));
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = pointValidation(m_score1, m_score2);
         }
         return score;
     }
+
+    private boolean sameScore() {
+        return m_score1==m_score2;
+    }
+
+    private boolean isOver4() {
+        return m_score1>=4 || m_score2>=4;
+    }
+
+    public String areEquals(int m_score1){
+        List<String> messages = List.of("Love-All", "Fifteen-All", "Thirty-All", "Deuce");
+        return (m_score1 > 2) ? messages.get(3) : messages.get(m_score1) ;
+    }
+
+    public String over4Score(int minusResult) {
+        switch (minusResult){
+            case 1:
+                return "Advantage player1";
+            case -1:
+                return "Advantage player2";
+            default:
+                return textResultOver2(minusResult);
+        }
+
+    }
+
+    private int getMinusResult(int m_score1, int m_score2) {
+        int minusResult;
+        minusResult = m_score1 - m_score2;
+        return minusResult;
+    }
+
+    private String textResultOver2(int minusResult) {
+        return minusResult >= 2 ? "Win for player1" : "Win for player2";
+    }
+
+    private String pointValidation(int m_score1, int m_score2) {
+        List<String> message = List.of("Love", "Fifteen", "Thirty", "Forty");
+        return message.get(m_score1) + "-" + message.get(m_score2);
+    }
+
 }
