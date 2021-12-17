54d72dc0489e89525a3b3ee702f0607ead8ba269
Ralph Sliger <ralphsliger1@gmail.com>
2021-12-17 16:44:30 -0500

    game2

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,9 +1,10 @@
+import java.util.List;
 
 public class TennisGame2 implements TennisGame
 {
     public int P1point = 0;
     public int P2point = 0;
-    
+
     public String P1res = "";
     public String P2res = "";
     private String player1Name;
@@ -16,112 +17,197 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (P1point == P2point && P1point < 4)
+
+        score = getMessage(score, P1point, P2point);
+
+        score = getDeuce(score, P1point, P2point);
+
+        score = getAdvantageScoreBoard(score, P1point, P2point);
+
+        score = getScoreBoard(score, P1point, P2point, P1res, P2res);
+
+        score = AdvantagePlayer(score, P1point, P2point);
+
+        score = WinnerPlayer(score,  P1point,  P2point);
+
+        return score;
+    }
+
+    private String getMessage(String score, int P1point, int P2point) {
+        List<String> message = List.of("Love-All", "Fifteen-All", "Thirty-All");
+        if (areEqualsAndLessThan4(P1point, P2point))
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+            if(P1point<=2){
+                score = message.get(P1point);
+            }
+
         }
-        if (P1point==P2point && P1point>=3)
+        return score;
+    }
+
+    private boolean areEqualsAndLessThan4(int P1point, int P2point) {
+        return areEquals(P1point, P2point) && P1LessThan4(P1point);
+    }
+
+    private boolean P1LessThan4(int P1point) {
+        return P1point < 4;
+    }
+
+    private boolean areEquals(int P1point, int P2point) {
+        return P1point == P2point;
+    }
+
+    private String getDeuce(String score, int P1point, int P2point) {
+        if (isEqualAndGreaterThan3(P1point, P2point))
             score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
+        return score;
+    }
+
+    private boolean isEqualAndGreaterThan3(int P1point, int P2point) {
+        return P1point==P2point && P1point>=3;
+    }
+
+    private String getAdvantageScoreBoard(String score, int P1point, int P2point) {
+        List<String> message = List.of("Fifteen", "Thirty", "Forty");
+        if (greaterThanZeroAndEqualZero(P1point, P2point))
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+            score = getP1(score, P1point, message);
         }
-        if (P2point > 0 && P1point==0)
+        if (greaterThanZeroAndEqualZero(P2point, P1point))
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+            score = getP2(score, P1point, P2point, message);
         }
-        
-        if (P1point>P2point && P1point < 4)
+        return score;
+    }
+
+    private String getP2(String score, int P1point, int P2point, List<String> message) {
+        if(P1point<=2){
+            score =  "Love" + "-" +message.get(P2point) ;
+        }
+        return score;
+    }
+
+    private String getP1(String score, int P1point, List<String> message) {
+        if(P1point<=2){
+            score = message.get(P1point) + "-" + "Love";
+        }
+        return score;
+    }
+
+    private boolean greaterThanZeroAndEqualZero(int P1point, int P2point) {
+        return P1point > 0 && P2point==0;
+    }
+
+    private String getScoreBoard(String score, int P1point, int P2point, String P1res, String P2res) {
+        if ( PAisGreaterThanPB(P1point, P2point))
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+            score = getBoard(score, P1point, P2point, P1res,  P2res);
         }
-        if (P2point>P1point && P2point < 4)
+
+        if (PAisGreaterThanPB(P2point, P1point))
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+            score = getBoardP2(score, P1point, P2point, P1res,  P2res);
         }
-        
-        if (P1point > P2point && P2point >= 3)
+        return score;
+    }
+
+    private String getBoardP2(String score, int P1point, int P2point, String P1res, String P2res) {
+        P2res = getP2res(P2point, P2res);
+        P1res = getP1res(P1point, P1res);
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private String getP2res(int P2point, String P2res) {
+        if (P2point ==2)
+            P2res="Thirty";
+        if (P2point ==3)
+            P2res="Forty";
+        return P2res;
+    }
+
+    private String getP1res(int P1point, String P1res) {
+        if (P1point ==1)
+            P1res="Fifteen";
+        if (P1point ==2)
+            P1res="Thirty";
+        return P1res;
+    }
+
+    private String getBoard(String score, int P1point, int P2point, String P1res, String P2res) {
+        P2res = getP1res(P2point, P2res);
+        P1res = getP2res(P1point, P1res);
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private boolean PAisGreaterThanPB(int P1point, int P2point) {
+        return P1point>P2point && P1point < 4;
+    }
+
+    private String AdvantagePlayer(String score,int P1point, int P2point) {
+        if (PlayerAGreaterThanPlayerBANDPlayerLessThan3Points(P1point, P2point))
         {
             score = "Advantage player1";
         }
-        
-        if (P2point > P1point && P1point >= 3)
+
+        if (PlayerAGreaterThanPlayerBANDPlayerLessThan3Points(P2point, P1point))
         {
             score = "Advantage player2";
         }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+        return score;
+    }
+
+    private boolean PlayerAGreaterThanPlayerBANDPlayerLessThan3Points(int P1point, int P2point) {
+        return P1point > P2point && P2point >= 3;
+    }
+
+    private String WinnerPlayer(String score, int P1point, int P2point) {
+        if (WinPlayer(P1point, P2point))
         {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (WinPlayer(P2point, P1point))
         {
             score = "Win for player2";
         }
         return score;
     }
-    
+
+    private boolean WinPlayer(int P1point, int P2point) {
+        return GreaterOrEquals4(P1point, P2point) && PlayerAminusPlayerB(P1point, P2point) >= 2;
+    }
+
+    private int PlayerAminusPlayerB(int P1point, int P2point) {
+        return P1point - P2point;
+    }
+
+    private boolean GreaterOrEquals4(int P1point, int P2point) {
+        return P1point >= 4 && P2point >= 0;
+    }
+
     public void SetP1Score(int number){
-        
+
         for (int i = 0; i < number; i++)
         {
             P1Score();
         }
-            
+
     }
-    
+
     public void SetP2Score(int number){
-        
+
         for (int i = 0; i < number; i++)
         {
             P2Score();
         }
-            
+
     }
-    
+
     public void P1Score(){
         P1point++;
     }
-    
+
     public void P2Score(){
         P2point++;
     }
